b2efc72e83e581045f45682e0c4d68659ea50967
Ikhsan Haikal <47350060+ikhsanhaikal@users.noreply.github.com>
2022-08-20 15:23:13 +0700

    Initial Commit

---
+++ uikit-nba-app/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  uikit-nba-app
+//
+//  Created by Ikhsan on 20/08/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
