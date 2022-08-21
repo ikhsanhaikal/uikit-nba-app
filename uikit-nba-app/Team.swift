ebc03409613a9afb758216568bae19babfaf0599
Ikhsan Haikal <47350060+ikhsanhaikal@users.noreply.github.com>
2022-08-21 08:08:52 +0700

    added team model

---
+++ uikit-nba-app/Team.swift
@@ -0,0 +1,32 @@
+//
+//  Team.swift
+//  uikit-nba-app
+//
+//  Created by Ikhsan on 20/08/2022.
+//
+
+import Foundation
+
+struct Team: Decodable {
+    let teamId: Int
+    let nbaDotComTeamId: Int
+    let key: String
+    let city: String
+    let conference: String
+    let division: String
+    let primaryColor: String
+    let secondaryColor: String
+    let wikipediaLogoUrl: String
+    
+    enum CodingKeys: String, CodingKey {
+        case teamId = "TeamID"
+        case nbaDotComTeamId = "NbaDotComTeamID"
+        case key = "Key"
+        case city = "City"
+        case conference = "Conference"
+        case division = "Division"
+        case primaryColor = "PrimaryColor"
+        case secondaryColor = "SecondaryColor"
+        case wikipediaLogoUrl = "WikipediaLogoUrl"
+    }
+}
